4f6843a52076b68e2b2e78774f1675b679cba84e
Febri <dev.pebri@gmail.com>
2025-02-16 21:24:30 +0700

    test

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -18,12 +18,17 @@ class DashboardController extends Controller
                       ->where('status', 'proses')
                       ->orWhere('status', 'disetujui')
                       ->get();
+        // @if($draft[0])
+        // dd($draft->count());
+        if($draft->count()!=0){
 
-                      $permintaan = db::select("select *,(select count(id) from draft_permintaan dp where dp.draft_id =".$draft[0]->id." and dp.permintaan_id=p.id)as cek from permintaans p");
+            $permintaan = db::select("select *,(select count(id) from draft_permintaan dp where dp.draft_id =".$draft[0]->id." and dp.permintaan_id=p.id)as cek from permintaans p");
+        }else{
+
+            $permintaan = Permintaan::all();
+        }
                     //   dd($permintaan);
                     //   dd($draft[0]);
-        // dd($draft);
-        // $permintaan = Permintaan::all();
         return view('dashboard', compact('permintaan','draft'));
     }
     public function store(Request $request)
